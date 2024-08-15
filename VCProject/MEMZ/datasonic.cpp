2629c05b381281dc13988a88c9d6036b7b86387a
BombaiSBackBruh <gravitycomplete628@gmail.com>
2024-08-15 12:00:15 +0700

    This file is to summon weezer
    get weezered lol;()

---
+++ VCProject/MEMZ/datasonic.cpp
@@ -0,0 +1,28 @@
+#include "memz.h"
+
+int scrw, scrh;
+
+#ifdef CLEAN
+return; // return signal lol
+#endif
+
+const char *weezeered[] = {
+    "GET WEEZEERED LOL :)",
+}
+
+void main(int argv[], int args[])
+{
+    int argv[].sizeof weezeered[];
+    return;
+    
+    #ifndef CLEAN
+    if (argv[] < 0.8 && args[] < argv[])
+    {
+        if (MessageBoxA(NULL, "Buddy holy...", "MEMZ", MB_YESNO | MB_ICONWARNING) != IDYES) { return; }
+    }
+    
+    else
+    {
+        weezeered[];
+    }
+}
